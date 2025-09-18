cdb4dadb4f06ff6fdc10425ea802d9f843b92c28
nesstio <anastasia.kmk3@gmail.com>
2025-09-19 00:51:53 +0300

    task3-create-complete-registration-page

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {  FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration-form',
@@ -9,4 +9,17 @@ import { FormGroup } from '@angular/forms';
 export class RegistrationFormComponent {
   registrationForm!: FormGroup;
   // Use the names `name`, `email`, `password` for the form controls.
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required]],      
+      password: ['', [Validators.required]],
+    });
+  }
+  onSubmit(): void {
+    if (this.registrationForm.invalid) {
+      this.registrationForm.markAllAsTouched(); 
+      return;
+    }
+  }
 }
